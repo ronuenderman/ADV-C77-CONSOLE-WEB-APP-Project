e2a08639b10bb0733d008792a48e176f19a00258
ronuenderman <105203798+ronuenderman@users.noreply.github.com>
2022-06-14 00:28:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Ekadri ";
+var last_name = "Sarkar";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var c = a + b;
+console.log(c);
